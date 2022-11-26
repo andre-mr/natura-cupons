32ce1d77434b4152f77335fa9076e9d6070d8500
Andre-MR <andreinternet@gmail.com>
2022-11-26 01:01:50 -0300

    dealing with instagram in-app browser

--- redirect/main.js
+++ redirect/main.js
@@ -1,6 +1,6 @@
 // replace with real domain
 const apiURL = "http://localhost:3000";
-const publicKey = "";
+const publicKey = "devkey";
 
 class PageConfigs {
   backgroundColor = "";
@@ -11,11 +11,31 @@ class PageConfigs {
   textColor = "";
 }
 
-const params = new URL(document.location).searchParams;
-let targetURL = params.get("url");
+let targetURL;
+let params = new URL(window.location).searchParams;
+
+if (!params) {
+  params = new URL(document.location).searchParams;
+}
+
+if (params) {
+  targetURL = params.get("url");
+} else {
+  targetURL = window.location.href.substring(
+    window.location.href.indexOf("url=") + 4,
+    window.location.href.length
+  );
+  if (!targetURL) {
+    targetURL = document.location.href.substring(
+      document.location.href.indexOf("url=") + 4,
+      document.location.href.length
+    );
+  }
+}
 
 let couponCode;
 let pageConfigs = new PageConfigs();
+let instagramAndroid = false;
 
 const topImage = document.getElementById("topImage");
 const contentText = document.getElementById("contentText");
@@ -51,21 +71,80 @@ async function applyPageConfigs() {
   contentText.style.setProperty("color", `${pageConfigs.textColor}`);
   codeButton.style.setProperty("border-color", `${pageConfigs.buttonColor}`);
   goButton.style.setProperty("background-color", `${pageConfigs.buttonColor}`);
+
+  await instagramBrowserAdapt();
 }
 
 async function goProduct() {
   await copyCode();
-  location.replace(targetURL);
+  if (instagramAndroid) {
+    // alert(
+    //   'Por favor, toque no menu acima e escolha a opção "Abrir no Chrome".'
+    // );
+  } else {
+    await copyCode();
+    try {
+      window.location.replace(targetURL);
+    } catch (e) {
+      window.location = targetURL;
+    }
+  }
 }
 
 async function copyCode() {
-  await navigator.clipboard.writeText(couponCode);
+  if (navigator.clipboard) {
+    //Chrome
+    await navigator.clipboard.writeText(couponCode);
+  } else if (window.clipboardData) {
+    // Internet Explorer
+    await window.clipboardData.setData("Text", couponCode);
+  } else {
+    // Old
+    const textArea = document.createElement("textarea");
+    textArea.value = couponCode;
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+      document.execCommand("copy");
+    } catch (err) {
+      console.error("Unable to copy", err);
+    }
+
+    document.body.removeChild(textArea);
+  }
+}
+
+async function instagramBrowserAdapt() {
+  if (instagramAndroid) {
+    contentText.innerText =
+      "Utilize o cupom acima no site para um desconto especial.";
+    const codeButtonText = codeButton.innerText;
+    codeButton.innerHTML = `<a href="${targetURL}">${codeButtonText}</a>`;
+    goButton.innerHTML = `<a href="${targetURL}">Ir à loja</a>`;
+  }
 }
 
 async function startup() {
+  if (
+    navigator.userAgent.includes("Instagram") &&
+    (navigator.userAgent.includes("Android") ||
+      !navigator.userAgent.includes("iPhone"))
+  ) {
+    instagramAndroid = true;
+  }
   await getPageConfigs();
   if (!targetURL) {
-    location.replace(pageConfigs.defaultTarget);
+    try {
+      window.location.replace(pageConfigs.defaultTarget);
+    } catch (e) {
+      window.location = pageConfigs.defaultTarget;
+    }
   } else {
     await getCoupon();
     applyPageConfigs();
